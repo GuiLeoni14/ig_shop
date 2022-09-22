ecf66507d4df11a379de191e3e545eb12db25d64
GuiLeoni14 <guileone167@gmail.com>
2022-09-22 16:34:49 -0300

    configurando estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import "../styles/globals.css";
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
 
-function MyApp({ Component, pageProps }) {
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
