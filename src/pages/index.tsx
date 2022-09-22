433ac133682183b6ec8f93fc7c41ae09a2b237d5
GuiLeoni14 <guileone167@gmail.com>
2022-09-22 18:42:06 -0300

    adicionando keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,42 @@
 import Image from "next/future/image";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
 import * as S from "../styles/pages/home";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <S.Container>
-      <S.Product>
+    <S.Container ref={sliderRef} className="keen-slider">
+      <S.Product className="keen-slider__slide">
         <Image src="/camisetas/1.png" width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
       </S.Product>
-      <S.Product>
-        <Image src="/camisetas/1.png" width={520} height={480} alt="" />
+      <S.Product className="keen-slider__slide">
+        <Image src="/camisetas/2.png" width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+      <S.Product className="keen-slider__slide">
+        <Image src="/camisetas/3.png" width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+      <S.Product className="keen-slider__slide">
+        <Image src="/camisetas/3.png" width={520} height={480} alt="" />
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,19 +2,18 @@ import { styled } from "..";
 
 export const Container = styled("main", {
   display: "flex",
-  gap: "3rem",
   width: "100%",
   maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
   marginLeft: "auto",
   minHeight: 656,
 });
 
-export const Product = styled("div", {
+export const Product = styled("a", {
   background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
+  overflow: "hidden",
 
   display: "flex",
   alignItems: "center",
@@ -30,7 +29,6 @@ export const Product = styled("div", {
     left: "0.25rem",
     right: "0.25rem",
     padding: "2rem",
-    overflow: "hidden",
     borderRadius: 6,
 
     display: "flex",
