8bc2aa8ffb44c42a7f8a2c58f140837f4d51496e
GuiLeoni14 <guileone167@gmail.com>
2022-09-22 18:30:08 -0300

    ajustando home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
-
+import Image from "next/future/image";
+import * as S from "../styles/pages/app";
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <S.Container>
+      <S.Header>
+        <Image src="/assets/logo.svg" alt="logo" width={140} height={52} />
+      </S.Header>
+      <Component {...pageProps} />
+    </S.Container>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
-import { styled } from "../styles";
-
-const Button = styled("button", {
-  backgroundColor: "$rocketseat",
-  borderRadius: 10,
-  span: {
-    fontWeight: "bold",
-  },
-  "&:hover": {
-    backgroundColor: "#000",
-  },
-});
+import Image from "next/future/image";
+import * as S from "../styles/pages/home";
 
 export default function Home() {
   return (
-    <div>
-      Home
-      <Button>
-        <span>Teste</span>Enviar
-      </Button>
-    </div>
+    <S.Container>
+      <S.Product>
+        <Image src="/camisetas/1.png" width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+      <S.Product>
+        <Image src="/camisetas/1.png" width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+    </S.Container>
   );
 }
